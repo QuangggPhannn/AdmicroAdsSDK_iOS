f4942a62ca0154177e54266186e29a7ac93915c0
Phan Quang <quangphanvan@vccorp.vn>
2026-02-05 11:22:03 +0700

    ver 1.0.0 SPM

--- Package.swift
+++ Package.swift
@@ -4,23 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary",
+    name: "AdmicroAdsSDK_iOS",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary"]
-        ),
+            name: "AdmicroAdsSDK_iOS",
+            targets: ["AdmicroAdsSDK_iOS"]),
+    ],
+    dependencies: [
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "MyLibrary"
-        ),
-        .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]
-        ),
+        .binaryTarget(name: "AdmicroAdsSDK_iOS",
+                      url: "https://github.com/QuangggPhannn/AdmicroAdsSDK_iOS/releases/download/1.0.0/AdmicroAdsSDK_iOS_1.0.0.zip",
+                      checksum: "f5383187c256c77799a53cfffa0dbe5608855024ddbd6d1f4cf09658ed9442b3"),
     ]
 )
